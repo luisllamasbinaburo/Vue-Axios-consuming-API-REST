0dc1f53f91f4bad12b0c30b894ff7f14ab16f1fe
Luis Llamas <luisllamas@hotmail.com>
2019-07-23 19:42:35 +0200

    Update API.js

--- API.js
+++ API.js
@@ -1,81 +1,94 @@
 function API_getRequest(id) {
-	axios.get('http://localhost:8080/items/' + id)
-		.then(function (response) {
-			console.log(response);
-		})
-		.catch(function (error) {
-			console.log(error);
-		})
-		.then(function () {
-		});
-}
-
-function API_putRequest(id, data) {
-	axios.put('http://localhost:8080/items', {
-		id: id,
-		data: data
-	})
-		.then(function (response) {
-			console.log(response);
-		})
-		.catch(function (error) {
-			console.log(error);
-		})
-		.then(function () {
-		});
-}
-
-function API_deleteRequest(id) {
-	axios.delete('http://localhost:8080/items/' + id)
-		.then(function (response) {
-			console.log(response);
-		})
-		.catch(function (error) {
-			console.log(error);
-		})
-		.then(function () {
-		});
-}
-
-
-function API_getAllRequest() {
-	axios.get('http://localhost:8080/items')
-		.then(function (response) {
-			console.log(response);
-		})
-		.catch(function (error) {
-			console.log(error);
-		})
-		.then(function () {
-		});
+    axios.get('http://localhost:8080/item/' + id)
+    .then(function (response) {
+        console.log(response);
+    })
+    .catch(function (error) {
+        console.log(error);
+    })
+    .then(function () {
+    });
 }
 
 function API_getFilteredRequest(filter) {
-	axios.get('http://localhost:8080/items', {
-		params: {
-			filter: filter
-		}
-	})
-		.then(function (response) {
-			console.log(response);
-		})
-		.catch(function (error) {
-			console.log(error);
-		})
-		.then(function () {
-		});
+    axios.get('http://localhost:8080/item', {
+        params: {
+            filter: filter
+        }
+    })
+    .then(function (response) {
+        console.log(response);
+    })
+    .catch(function (error) {
+        console.log(error);
+    })
+    .then(function () {
+    });
+}
+
+function API_getAllRequest() {
+    axios.get('http://localhost:8080/item')
+    .then(function (response) {
+        console.log(response);
+    })
+    .catch(function (error) {
+        console.log(error);
+    })
+    .then(function () {
+    });
 }
 
 function API_postRequest(data) {
-	axios.post('http://localhost:8080/items', {
-		data: data
-	})
-		.then(function (response) {
-			console.log(response);
-		})
-		.catch(function (error) {
-			console.log(error);
-		})
-		.then(function () {
-		});
+    axios.post('http://localhost:8080/item', {
+        data: data
+    })
+    .then(function (response) {
+        console.log(response);
+    })
+    .catch(function (error) {
+        console.log(error);
+    })
+    .then(function () {
+    });
+}
+
+function API_putRequest(id, data) {
+    axios.put('http://localhost:8080/item/ ' + id, {
+        data: data
+    })
+    .then(function (response) {
+        console.log(response);
+    })
+    .catch(function (error) {
+        console.log(error);
+    })
+    .then(function () {
+    });
+}
+
+function API_patchRequest(id, data) {
+    axios.patch('http://localhost:8080/item/ ' + id, {
+        data: data
+    })
+    .then(function (response) {
+        console.log(response);
+    })
+    .catch(function (error) {
+        console.log(error);
+    })
+    .then(function () {
+    });
+}
+
+
+function API_deleteRequest(id) {
+    axios.delete('http://localhost:8080/item/' + id)
+    .then(function (response) {
+        console.log(response);
+    })
+    .catch(function (error) {
+        console.log(error);
+    })
+    .then(function () {
+    });
 }
